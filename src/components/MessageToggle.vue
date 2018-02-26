32345710e699d945c79066bea8555e95bb589f45
phoet <phoetmail@googlemail.com>
2018-02-26 16:45:46 +0100

    make it fail

--- src/components/MessageToggle.vue
+++ src/components/MessageToggle.vue
@@ -4,19 +4,24 @@
     <button id="toggle-message" @click="toggleMessage">
       Change message
     </button>
+    <img :src="image" />
   </div>
 </template>
 
 <script>
 import Message from './Message'
+import logo1 from '@/assets/logo1.png'
+import logo2 from '@/assets/logo2.png'
 
 export default {
   name: 'message-toggle',
   data: () => ({
-    msg: null
+    msg: null,
+    image: logo1
   }),
   methods: {
     toggleMessage () {
+      this.image = this.image === logo1 ? logo2 : logo1
       this.msg = this.msg === 'message' ? 'toggled message' : 'message'
     }
   },
